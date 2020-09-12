81163c17ef46dface60de28cef8a0e33dbcef6be
Rocio Batres <jonasgirl21@gmail.com>
2020-09-12 02:02:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,14 @@ def countdown(x)
   puts "HAPPY NEW YEAR!"
 end
 
-  
+def countdown_with_sleep(second)
+  while x > 0 
+    puts "#{second} SECOND(S)!"
+    sleep(5)
+    x -= 1
+  end  
+  puts"HAPPY NEW YEAR!"
+end  
 
 
 
