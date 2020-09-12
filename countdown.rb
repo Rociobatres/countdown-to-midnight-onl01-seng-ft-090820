30d2890c55b5ffa1771f21c4118ad5a809d80999
Rocio Batres <jonasgirl21@gmail.com>
2020-09-12 01:54:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
-end
+def countdown(x)
+  while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1 
+  while x == 0
+    puts "HAPPY NEW YEAR!"
+  end 
+end  
+
+
+def countdown_with_sleep(second)
+  while x > 0 
+    puts "#{second} SECOND(S)!"
+    x -= 1
+    sleep(10)
+  end  
+puts"HAPPY NEW YEAR!"
+end  
+
+
+
